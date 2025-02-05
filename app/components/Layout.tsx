887c188afd5cd9ffb7f604235150c4d4c5bb01bd
Md. Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2025-02-05 06:31:28 +0600

    navbar showing problem solve in signup and signin page

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,12 +1,25 @@
-import { Toaster } from 'react-hot-toast';
-import Navigation from './Navigation';
+"use client";
+import { Toaster } from "react-hot-toast";
+import Navigation from "./Navigation";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isauthenticated, setIsAuthenticated] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <Toaster position="top-right" />
-      <Navigation />
+      {isauthenticated && pathname !== "/login" && pathname !== "/signup" && (
+        <Navigation />
+      )}{" "}
       <main>{children}</main>
     </div>
   );
-} 
+}
